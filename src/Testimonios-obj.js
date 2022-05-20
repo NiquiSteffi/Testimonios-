759b15f44621655610d7990eaf0d3bdf57f5b26a
niquisteffi <niqui.steffi@hotmail.com>
2022-05-20 11:02:50 -0300

    subiendo proyecto

---
+++ src/Testimonios-obj.js
@@ -0,0 +1,13 @@
+const testimonios = {
+    "testimonio": [
+        {
+            nombre: "Shawn Wang",
+            pais: "Singapur",
+            cargo: "Ingeniero de Software",
+            empresa: "Amazon",
+            testimonio: "Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.",
+            imagen: "1"
+        }
+    ]
+};
+
